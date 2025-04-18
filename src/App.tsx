463fb58ebd9c35decda40bb45a7c6bcf2c7536df
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-04-18 16:53:43 -0300

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Button } from "./Button";
+
+export function App() {
+  return (
+   <>
+    <Button variant="primary" />
+    <Button variant="secondary" />
+    <Button variant="danger" />
+    <Button variant="success" />
+    <Button />
+   </>
+  )
+}
