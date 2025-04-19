154c1e44b974bff2eef25b2695b67c1bdc016298
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-04-19 20:40:56 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
-import { Button } from "./Button";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyle } from "./styles/global";
+import { defaultTheme } from "./styles/themes/default";
+
+import { Button } from "./components/Button";
 
 export function App() {
   return (
-   <>
+   <ThemeProvider theme={defaultTheme}>
     <Button variant="primary" />
     <Button variant="secondary" />
     <Button variant="danger" />
     <Button variant="success" />
     <Button />
-   </>
+
+    <GlobalStyle />
+   </ThemeProvider>
   )
 }
