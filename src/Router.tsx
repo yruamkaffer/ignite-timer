a7b1b8b8337b5cc0825dd4d8ba84c3606e61534f
Yruam Käffer de Faria <yruamkf@gmail.com>
2025-04-23 23:16:02 -0300

    Add files via upload

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { DefaultLayout } from './layouts/DefaultLayout/index.tsx';
-import { History } from './pages/History.tsx';
+import { History } from './pages/History/index.tsx';
 import { Home } from './pages/Home/index.tsx';
 
 export function Router() {
